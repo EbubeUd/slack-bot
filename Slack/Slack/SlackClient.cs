6cd61704b5aa5df2c8475310290bb564378a3741
kripsonud <kripsonud@gmail.com>
2020-12-10 13:58:43 +0100

    imitial commit

---
+++ Slack/Slack/SlackClient.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Slack
+{
+    public class SlackClient
+    {
+        private readonly Uri _webhookUrl;
+        private readonly HttpClient _httpClient = new HttpClient();
+ 
+        public SlackClient(Uri webhookUrl)
+        {
+            _webhookUrl = webhookUrl;
+        }
+ 
+        public async Task<HttpResponseMessage> SendMessageAsync(string message, string channel = null, string username = null)
+        { 
+            var payload = new
+            {
+            text = message,
+            channel,
+            username,
+             };
+            var serializedPayload = JsonConvert.SerializeObject(payload);
+            var response = await _httpClient.PostAsync(_webhookUrl,new StringContent(serializedPayload, Encoding.UTF8, "application/json"));
+ 
+            return response;
+        }
+    }
+}
